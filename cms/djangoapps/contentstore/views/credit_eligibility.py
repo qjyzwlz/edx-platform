a4ef0c18ddd1d35cb9548d6497bec114b55da059
zubair-arbi <zubair.afzal@arbisoft.com>
2015-05-21 15:47:36 +0500

    update creadit elgibilty checklists view

--- cms/djangoapps/contentstore/views/credit_eligibility.py
+++ cms/djangoapps/contentstore/views/credit_eligibility.py
@@ -1,15 +1,15 @@
 """
-Views related to operations on course objects
+Views related to credit eligibility criterion of a course.
 """
 
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_http_methods, require_GET
 from django.core.exceptions import PermissionDenied
+from django.views.decorators.http import require_GET
 
-from openedx.core.djangoapps.credit.api import get_credit_requirements
 from django_future.csrf import ensure_csrf_cookie
 from edxmako.shortcuts import render_to_response
 from opaque_keys.edx.keys import CourseKey
+from openedx.core.djangoapps.credit.api import get_credit_requirements
 from student.auth import has_course_author_access
 from xmodule.modulestore.django import modulestore
 
@@ -17,19 +17,16 @@ from xmodule.modulestore.django import modulestore
 __all__ = ['credit_eligibility_handler', ]
 
 
-# pylint: disable=unused-argument
 @ensure_csrf_cookie
-@require_http_methods("GET")
+@require_GET
 @login_required
 def credit_eligibility_handler(request, course_key_string):
     """
-    The restful handler for checklists.
+    The restful handler for credit eligibility checklists.
 
     GET
         html: return html page for all checklists
-        json: return json representing all checklists. checklist_index is not supported for GET at this time.
-    POST or PUT
-        json: updates the checked state for items within a particular checklist. checklist_index is required.
+        json: not currently supported
     """
 
     course_key = CourseKey.from_string(course_key_string)
@@ -37,15 +34,13 @@ def credit_eligibility_handler(request, course_key_string):
         raise PermissionDenied()
 
     course_module = modulestore().get_course(course_key)
-
-    json_request = 'application/json' in request.META.get('HTTP_ACCEPT', 'application/json')
     if request.method == 'GET':
-        # If course was created before checklists were introduced, copy them over
-        # from the template.
-
+        # get and display all credit eligibility requirements.
         requirements = get_credit_requirements(course_key)
-        return render_to_response('credit_eligibility.html',
-                                  {
-                                      'requirements': requirements,
-                                      'context_course': course_module,
-                                  })
+        return render_to_response(
+            'credit_eligibility.html',
+            {
+                'requirements': requirements,
+                'context_course': course_module,
+            }
+        )
