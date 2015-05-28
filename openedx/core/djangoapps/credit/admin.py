e54f438e3f36a9defc3896c8aab5d9cc50af0014
Ahsan Ulhaq <ahsan.haq@arbisoft.com>
2015-05-28 17:00:35 +0500

    Set up Credit Provider Model
    Update credit Provider model
    
    ECOM-1555

--- openedx/core/djangoapps/credit/admin.py
+++ openedx/core/djangoapps/credit/admin.py
@@ -2,7 +2,14 @@
 Django admin page for credit eligibility
 """
 from ratelimitbackend import admin
-from .models import CreditCourse
+from .models import CreditCourse, CreditProvider
 
+
+class CreditProviderModelAdmin(admin.ModelAdmin):
+    """
+    Admin for `CreditProvider` model
+    """
+    pass
 
 admin.site.register(CreditCourse)
+admin.site.register(CreditProvider, CreditProviderModelAdmin)
