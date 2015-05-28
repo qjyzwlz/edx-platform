d29e8c3e4c0e53186efab5b39ac8777d77d83d83
aamir-khan <aamir@edx.org>
2015-05-28 18:07:25 +0500

    ECOM-1592: Adding incourse reverification as a credit requirement in course

--- openedx/core/djangoapps/credit/tasks.py
+++ openedx/core/djangoapps/credit/tasks.py
@@ -33,15 +33,7 @@ def update_course_requirements(course_id):
         is_credit_course = CreditCourse.is_credit_course(course_key)
         if is_credit_course:
             course = modulestore().get_course(course_key)
-            requirements = [
-                {
-                    "namespace": "grade",
-                    "name": "grade",
-                    "criteria": {
-                        "min_grade": get_min_grade_for_credit(course)
-                    }
-                }
-            ]
+            requirements = _get_course_credit_requirements(course)
             set_credit_requirements(course_key, requirements)
     except (InvalidKeyError, ItemNotFoundError, InvalidCreditRequirements) as exc:
         LOGGER.error('Error on adding the requirements for course %s - %s', course_id, unicode(exc))
@@ -50,6 +42,93 @@ def update_course_requirements(course_id):
         LOGGER.info('Requirements added for course %s', course_id)
 
 
-def get_min_grade_for_credit(course):
-    """ Returns the min_grade for the credit requirements """
-    return getattr(course, "min_grade", 0.8)
+def _get_min_grade_for_credit(course):
+    """ Returns the min_grade for the credit requirements
+
+     Args:
+        course(Course): The course object
+
+    Returns:
+        Float value of minimum_grade_credit attribute of course
+    """
+    return getattr(course, "minimum_grade_credit", 0.8)
+
+def _get_course_credit_requirements(course):
+    """ Returns the list of credit requirements for the given course
+
+    It returns the minimum_grade_credit and also the ICRV checkpoints
+    if added any in the course
+
+    Args:
+        course(Course): The course object
+
+    Returns:
+        List of minimum_grade_credit and ICRV requirements
+    """
+    icrv_requirements = _get_credit_course_requirements_xblocks(course)
+    min_grade_requirement = _get_min_grade_requirement(course)
+    icrv_requirements.extend(min_grade_requirement)
+    return icrv_requirements
+
+
+def _get_min_grade_requirement(course):
+    """ Returns the list of minimum_grade_credit requirements for the given course
+
+    Args:
+        course(Course): The course object
+
+    Returns:
+        The list of minimum_grade_credit requirements
+    """
+    requirement = [
+        {
+            "namespace": "grade",
+            "name": "grade",
+            "criteria": {
+                "min_grade": _get_min_grade_for_credit(course)
+            }
+        }
+    ]
+    return requirement
+
+
+def _get_credit_course_requirements_xblocks(course):  # pylint: disable=invalid-name
+    """ Generates a course structure dictionary for the specified course.
+
+    Args:
+        course(Course): The course object
+
+    Returns:
+        The list of credit requirements xblocks dicts
+    """
+
+    blocks_stack = [course]
+    requirements_blocks = []
+    while blocks_stack:
+        curr_block = blocks_stack.pop()
+        children = curr_block.get_children() if curr_block.has_children else []
+        if _is_credit_requirement(curr_block):
+            block = {
+                "namespace": curr_block.get_credit_requirement_namespace(),
+                "name": curr_block.get_credit_requirement_name(),
+                "criteria": ""
+            }
+            requirements_blocks.append(block)
+
+        # Add this blocks children to the stack so that we can traverse them as well.
+        blocks_stack.extend(children)
+    return requirements_blocks
+
+
+def _is_credit_requirement(xblock):
+    """ Check if the given xblock is a credit requirement
+
+    Args:
+        xblock(XBlock): The given xblock object
+
+    Returns:
+        True if xblock is a credit requirement else False
+    """
+    if callable(getattr(xblock, "is_course_credit_requirement", None)):
+        return xblock.is_course_credit_requirement()
+    return False
