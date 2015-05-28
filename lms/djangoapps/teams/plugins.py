3e5a88e08a60d384bebe72930e151a00bccafc0c
cahrens <christina@edx.org>
2015-05-28 12:23:30 -0400

    Update to newest version of pluggable tabs API.

--- lms/djangoapps/teams/plugins.py
+++ lms/djangoapps/teams/plugins.py
@@ -3,15 +3,15 @@ Definition of the course team feature.
 """
 
 from django.utils.translation import ugettext as _
-from openedx.core.lib.plugins.api import CourseViewType
+from courseware.tabs import EnrolledCourseViewType
 
 
-class TeamsCourseViewType(CourseViewType):
+class TeamsCourseViewType(EnrolledCourseViewType):
     """
     The representation of the course teams view type.
     """
 
-    name = "edx.teams"
+    name = "teams"
     title = _("Teams")
     view_name = "teams_dashboard"
 
@@ -24,5 +24,8 @@ class TeamsCourseViewType(CourseViewType):
             settings (dict): a dict of configuration settings
             user (User): the user interacting with the course
         """
+        if not super(TeamsCourseViewType, cls).is_enabled(course, settings, user=user):
+            return False
+
         # TODO: the tab is only enabled if at least one team topic is defined
         return not user or settings.FEATURES.get('ENABLE_TEAMS', False) or not course.teams_enabled
