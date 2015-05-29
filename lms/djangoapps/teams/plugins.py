7415b84c35eff3b06cd3eac723af90dc13a5f5ba
Andy Armstrong <andya@edx.org>
2015-05-29 00:35:42 -0400

    Update is_enabled method

--- lms/djangoapps/teams/plugins.py
+++ lms/djangoapps/teams/plugins.py
@@ -2,6 +2,7 @@
 Definition of the course team feature.
 """
 
+from django.conf import settings
 from django.utils.translation import ugettext as _
 from courseware.tabs import EnrolledCourseViewType
 
@@ -16,7 +17,7 @@ class TeamsCourseViewType(EnrolledCourseViewType):
     view_name = "teams_dashboard"
 
     @classmethod
-    def is_enabled(cls, course, settings, user=None):  # pylint: disable=unused-argument
+    def is_enabled(cls, course, user=None):  # pylint: disable=unused-argument
         """Returns true if the teams feature is enabled in the course.
 
         Args:
@@ -24,8 +25,7 @@ class TeamsCourseViewType(EnrolledCourseViewType):
             settings (dict): a dict of configuration settings
             user (User): the user interacting with the course
         """
-        if not super(TeamsCourseViewType, cls).is_enabled(course, settings, user=user):
+        if not super(TeamsCourseViewType, cls).is_enabled(course, user=user):
             return False
 
-        # TODO: the tab is only enabled if at least one team topic is defined
-        return not user or settings.FEATURES.get('ENABLE_TEAMS', False) or not course.teams_enabled
+        return settings.FEATURES.get('ENABLE_TEAMS', False) and course.teams_enabled
