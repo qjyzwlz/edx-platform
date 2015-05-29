afb8af66132222dd6e1c396a97b6c33bc497f83e
cahrens <christina@edx.org>
2015-05-29 13:49:27 -0400

    unit test for views

--- lms/djangoapps/teams/views.py
+++ lms/djangoapps/teams/views.py
@@ -4,26 +4,29 @@ View methods for the course team feature.
 
 from django.shortcuts import render_to_response
 from opaque_keys.edx.keys import CourseKey
-from courseware.courses import get_course_with_access
+from courseware.courses import get_course_with_access, has_access
 from django.http import Http404
 from django.conf import settings
+from student.models import CourseEnrollment
 
 
 def teams_dashboard(request, course_id):
     """
     Renders the teams dashboard, which is shown on the "Teams" tab.
+
+    Raises a 404 if the user is not registered for the course, or if the teams feature is not enabled.
     """
-    # TODO: add unit tests for these conditions
     course_key = CourseKey.from_string(course_id)
     course = get_course_with_access(request.user, "load", course_key)
 
     if not is_feature_enabled(course):
         raise Http404
 
-    context = {
-        "course": course,
-    }
+    if not CourseEnrollment.is_enrolled(request.user, course.id) and \
+            not has_access(request.user, 'staff', course, course.id):
+        raise Http404
 
+    context = {"course": course}
     return render_to_response("teams/teams.html", context)
 
 
